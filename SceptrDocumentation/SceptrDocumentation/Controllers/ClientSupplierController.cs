4932e0a424ec49bca585c1ebb9138de1c9cceefa
dsaraf@tavisca.com <dsaraf@tavisca.com>
2014-11-16 17:30:02 +0530

    Added Implementation to Modify client and supplier mapping page.Partially Correct.Need Change in rendering logic

--- SceptrDocumentation/SceptrDocumentation/Controllers/ClientSupplierController.cs
+++ SceptrDocumentation/SceptrDocumentation/Controllers/ClientSupplierController.cs
@@ -127,6 +127,78 @@ namespace SceptrDocumentation.Controllers
             return RedirectToAction("Index");
         }
 
+
+        [AcceptVerbs(HttpVerbs.Get)]
+        public JsonResult LoadProductsBySuppliers(string sId)
+        {
+            int id = int.Parse(sId);
+            var productList = (from suppProd in db.SupplierProducts.Include(s => db.Suppliers)
+                                                               .Include(p => db.Products)
+                               where suppProd.Supplier.ID == id
+                               select suppProd.ProductId).ToList();
+
+
+            var products = new List<string>();
+            foreach (var productId in productList)
+            {
+                var name = (from prod in db.Products where prod.ID == productId select prod.Name).First();
+                products.Add(name);
+            }
+            var phyData = products.Select(m => new SelectListItem()
+            {
+                Text = m,
+                Value = m,
+            });
+            return Json(phyData, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult SeeDetails(int id=0)
+        {
+            var suppliers = (from suppProd in db.SupplierProducts.Include(s => db.Suppliers)
+                                                                .Include(p => db.Products)
+                             where suppProd.Product.ID == id
+                             select suppProd.SupplierId).ToList();
+
+
+
+            var supplierNames = new List<string>();
+            var clientNames = new List<string>();
+            var clientsSuppliers = new List<string>();
+            foreach (var supplier in suppliers)
+            {
+
+                var clientId = (from cs in db.ClientSupplierMaps.Include(s => db.Suppliers)
+                                           .Include(c => db.Clients)
+                                       where cs.SupplierId == supplier && cs.ProductId == id
+                                       select cs.ClientId).ToList();
+                var supplierName = (from supp in db.Suppliers where supp.ID == supplier select supp.Name).First();
+                supplierNames.Add(supplierName);
+                foreach (var cId in clientId)
+                {
+                    var clientName = (from client in db.Clients where client.ID == cId select client.Name).First();
+                    if (!clientNames.Contains(clientName))
+                    {
+                        clientNames.Add(clientName);
+                    }
+                    
+                    var csName = clientName +"+"+ supplierName;
+                    clientsSuppliers.Add(csName);
+                }
+            }
+
+
+            //var clientsuppliermaps = (from csm in db.ClientSupplierMaps.Include(s => db.Suppliers)
+            //                                                            .Include(c => db.Clients)
+            //                                                            .Include(p=>db.Products) 
+            //                                                    where csm.ProductId == id 
+            //                                                    select csm).ToList();
+            ViewBag.Suppliers = supplierNames;
+            ViewBag.Clients = clientNames;
+            ViewBag.ClientSuppliers = clientsSuppliers;
+            return View();
+        }
+
+
         protected override void Dispose(bool disposing)
         {
             db.Dispose();
